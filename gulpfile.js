00ef867c2b263309c265061bab18a26ae707ed0d
Alex Piliugin <o.piliugin@gmail.com>
2016-12-23 11:27:20 +0200

    Added some files

---
+++ gulpfile.js
@@ -0,0 +1,41 @@
+var gulp = require("gulp"),
+	connect = require("gulp-connect"),
+	opn = require("opn");
+
+//запуск сервера
+gulp.task('connect', function() {
+	connect.server({
+		root: 'app',
+		livereload: true,
+		port: 8888
+	});
+	opn('http://localhost:8888');
+});
+
+//Работа с html
+gulp.task('html', function() {
+	gulp.src('./app/*.html')
+		.pipe(connect.reload());	
+});
+
+//Работа с CSS
+gulp.task('css', function() {
+	gulp.src('./app/css/*.css')
+		.pipe(connect.reload());
+});
+
+//Работа с JS
+gulp.task('js', function() {
+	gulp.src('./app/js/*.js')
+		.pipe(connect.reload());
+});
+
+//Слежка
+gulp.task('watch', function() {
+	gulp.watch(['./app/*.html'], ['html']);
+	gulp.watch(['./app/*.css'], ['css']);
+	gulp.watch(['./app/*.js'], ['js']);
+});
+
+//Задача по умолчанию
+gulp.task('default', ['connect', 'watch']);
